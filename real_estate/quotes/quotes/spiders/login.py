02530235547ef8851e295f74645150f180bfcf67
meevaldas <meevadas13@gmail.com>
2023-07-30 14:06:41 +0530

    Initial commit

---
+++ real_estate/quotes/quotes/spiders/login.py
@@ -0,0 +1,19 @@
+import scrapy
+from scrapy import Request, FormRequest
+
+
+class LoginSpider(scrapy.Spider):
+    name = "login"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com"]
+
+    def parse_login(self, response):
+        ret = FormRequest.from_response(response, formdata={"username": "ahmed", "password": "mypass"})
+        self.log("Sent:" + str(ret.body))
+        return ret
+
+    def start_requests(self):
+        return [Request("https://quotes.toscrape.com/login", callback=self.parse_login)]
+
+    def parse(self, response, **kwargs):
+        pass
